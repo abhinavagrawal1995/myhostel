240d46a0f7b69df47bc3bd50a92cf1a511ab8b82
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-06-18 12:59:58 +0530

    auto push

--- dbkaConn.php
+++ dbkaConn.php
@@ -3,7 +3,6 @@ include 'keys.php';
 // Create connection
 $con=mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (mysqli_connect_errno()) {
-  echo "This is embarassing. Looks like we ran into some problems. Please try again later. " . mysqli_connect_error();
+  die("This is embarassing. Looks like we ran into some problems. Please try again later. " . mysqli_connect_error());
 }
-else echo "Asd";
 ?>
